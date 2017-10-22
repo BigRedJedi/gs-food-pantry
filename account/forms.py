d7df19a0e409ada52cccd7dcd4640f42833762c6
kthampton <kthampton@unomaha.edu>
2017-10-22 14:47:12 -0500

    Updated HTML files

--- account/forms.py
+++ account/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import *
 
 
-
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
 
 class ClientForm(forms.ModelForm):
     class Meta:
