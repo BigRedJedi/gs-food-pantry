ce872f3d3372ea69fc1838ec756cf2de2d24a0d9
kthampton <kthampton@unomaha.edu>
2017-11-11 14:56:23 -0600

    Updates for Heroku push test

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,6 @@ from cart.forms import CartAddProductForm
 
 # Added per Cart (see page 229)
 
-<<<<<<< HEAD
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
@@ -32,8 +31,7 @@ def product_detail(request, id, slug):
     return render(request,
                         'shop/product/detail.html',
                         {'product': product})
-=======
->>>>>>> parent of abe0ece... Export to CSV functionality added, PDF print functionality in progress
+
 
 def product_list(request, category_slug=None):
     category = None
