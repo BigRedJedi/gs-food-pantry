44b0f6015cc0e043f97ee543dc5ac877fa92e149
nithesh1510 <nrenikunta@unomaha.edu>
2017-11-11 02:14:37 -0600

    updated the shop part,removed price of products from entire website: shop,cart are working. Must check on the checkout page

--- shop/admin.py
+++ shop/admin.py
@@ -9,9 +9,9 @@ admin.site.register(Category, CategoryAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'stock',
+    list_display = ['name', 'slug', 'stock',
                     'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'stock', 'available']
+    list_editable = [ 'stock', 'available']
     prepopulated_fields = {'slug': ('name',)}
 admin.site.register(Product, ProductAdmin)
