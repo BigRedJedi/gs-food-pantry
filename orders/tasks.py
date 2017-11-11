66d3cce0a1d3bfb508f8a178b30e1c6644178877
kthampton <kthampton@unomaha.edu>
2017-11-11 12:22:28 -0600

    Updated tasks.py

--- orders/tasks.py
+++ orders/tasks.py
@@ -2,6 +2,7 @@ from celery import task
 from django.core.mail import send_mail
 from .models import Order
 
+
 @task
 def order_created(order_id):
     """
@@ -11,8 +12,7 @@ def order_created(order_id):
     order = Order.objects.get(id=order_id)
     subject = 'Order nr. {}'.format(order.id)
     message = 'Dear {},\n\nYou have successfully placed an order.\
-                  Your order id is {}.'.format(order.first_name,
-                                            order.id)
+                  Your order id is {}.'.format(order.first_name,order.id)
     mail_sent = send_mail(subject,
                           message,
                           'admin@myshop.com',
