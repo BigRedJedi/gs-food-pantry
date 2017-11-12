5486e7a23655ad661201e59cc2f6d3c79b3b6767
kthampton <kthampton@unomaha.edu>
2017-11-11 18:48:30 -0600

    Updated order create fields

--- orders/views.py
+++ orders/views.py
@@ -24,8 +24,7 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
-                                         quantity=item['quantity'])
+                OrderItem.objects.create(order=order, product=item['product'],quantity=item['quantity'])
             # clear the cart
             cart.clear()
             # launch asynchronous task
