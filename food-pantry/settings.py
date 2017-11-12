581ba4279bb4f28723ad1c30fe40490c6017c800
kthampton <kthampton@unomaha.edu>
2017-11-11 22:29:06 -0600

    Template updates for client visits

--- food-pantry/settings.py
+++ food-pantry/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 from django.core.urlresolvers import reverse_lazy
 
 from celery import Celery
+
 
 LOGIN_REDIRECT_URL = reverse_lazy('home')
 LOGIN_URL = reverse_lazy('login')
