a7f0e50958e8514eab771775b5366c6241bdbf2a
ajanet <31288748+ajanet379@users.noreply.github.com>
2017-11-12 19:45:11 -0600

    Linking Visit model and orders

--- orders/forms.py
+++ orders/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import Order
+from .models import Order, Visit
 
 
 class OrderCreateForm(forms.ModelForm):
 
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        fields = ['visit', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
