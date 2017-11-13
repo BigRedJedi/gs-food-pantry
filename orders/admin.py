7045ca92ff4b37b1b6d12332a255a643c594603b
kthampton <kthampton@unomaha.edu>
2017-11-13 13:20:53 -0600

    Reversions on visit code to fix order print function

--- orders/admin.py
+++ orders/admin.py
@@ -35,21 +35,23 @@ export_to_csv.short_description = 'Export to CSV'
 
 def order_detail(obj):
     return '<a href="{}">View</a>'.format(
-        reverse('orders:admin_order_detail', args=[obj.visit]))
+        reverse('orders:admin_order_detail', args=[obj.id]))
 order_detail.allow_tags = True
 
 
 def order_pdf(obj):
     return '<a href="{}">PDF</a>'.format(
-        reverse('orders:admin_order_pdf', args=[obj.visit]))
+        reverse('orders:admin_order_pdf', args=[obj.id]))
 order_pdf.allow_tags = True
 order_pdf.short_description = 'PDF bill'
 
 
 class OrderAdmin(admin.ModelAdmin):
     # Watch apostrophes
-    list_display = ['visit', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created',
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created',
                     'updated', order_detail]
+    # list_display = ['visit', 'id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created',
+    #                'updated', order_detail]
     # order_pdf, ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
--- orders/forms.py
+++ orders/forms.py
@@ -1,9 +1,11 @@
 from django import forms
-from .models import Order, Visit
+from .models import Order
+# , Visit
 
 
 class OrderCreateForm(forms.ModelForm):
 
     class Meta:
         model = Order
-        fields = ['visit', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        # fields = ['visit', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
